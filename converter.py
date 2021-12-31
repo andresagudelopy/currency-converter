66f67d0998e93f49c23efbd6eab9a8b44ad3b224
andresagudelopy <andresjagudelo90@gmail.com>
2021-12-31 01:37:09 -0500

    Agregamos el archivo conditionals.py

--- converter.py
+++ converter.py
@@ -2,6 +2,6 @@ pesos = input("¿Cuántos pesos colombianos tienes?: ")
 pesos = float(pesos)
 valor_dolar = 3875
 dolares = pesos / valor_dolar
-dolares = round(dolares, 2) # round sirve para redondear la cantidad de numeros decimales
+dolares = round(dolares, 2) # round nos sirve para redondear la cantidad de decimales
 dolares = str(dolares)
 print("Tienes $" + dolares + " dólares")
