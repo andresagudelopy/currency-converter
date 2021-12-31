db9d5cb7fa3bc49cc7b364915a3fbea478352565
andresagudelopy <andresjagudelo90@gmail.com>
2021-12-31 02:01:50 -0500

    Se agrega las divisas: pesos argentinos y pesos mexicanos

--- converter.py
+++ converter.py
@@ -1,7 +1,38 @@
-pesos = input("¿Cuántos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2) # round nos sirve para redondear la cantidad de decimales
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
+menu = """
+Welcome to the currency converter
+
+1 - Colombian pesos
+2 - Argentine pesos 
+3 - Mexican pesos
+
+Choose an option: """
+option = int(input(menu))
+if option == 1:
+    pesos = input("¿How many Colombian pesos do you have ?: ")
+    pesos = float(pesos)
+    dollar_value = 4042
+    dollars = pesos / dollar_value
+    dollars = round(dollars, 2) # round nos sirve para redondear la cantidad de decimales
+    dollars = str(dollars)
+    print("You have $" + dollars + " dollars")
+elif option == 2:
+    pesos = input("¿How many Argentine pesos do you have ?: ")
+    pesos = float(pesos)
+    dollar_value = 102
+    dollars = pesos / dollar_value
+    dollars = round(dollars, 2) # round nos sirve para redondear la cantidad de decimales
+    dollars = str(dollars)
+    print("You have $" + dollars + " dollars")
+elif option == 3:
+    pesos = input("¿How many Mexican pesos do you have ?: ")
+    pesos = float(pesos)
+    dollar_value = 20
+    dollars = pesos / dollar_value
+    dollars = round(dollars, 2) # round nos sirve para redondear la cantidad de decimales
+    dollars = str(dollars)
+    print("You have $" + dollars + " dollars")
+else:
+    print("Enter a correct option please")
+    
+
+
